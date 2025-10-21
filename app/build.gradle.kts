afe351a488e24f6587b79e1d5f90df1eff2b4c2b
Junyi Qi <junyi.qi@my.jcu.edu.au>
2025-10-21 19:48:18 +0800

    feat(build): configure build variants for debug and release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
